01c41bfd2113267433b946af89ab546d9b28d6c4
Shinra Zen <pratham18koli@gmail.com>
2026-01-16 20:08:56 +0530

    updating the files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css'  // Make sure this line exists
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
